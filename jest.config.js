183bde267c71179ccab11eb3b18f2b620f9b3755
Christopher Queen <christopher.queen@gmail.com>
2020-05-13 20:33:37 -0400

    Github workflow updates Coverage Updates

--- jest.config.js
+++ jest.config.js
@@ -19,13 +19,13 @@ module.exports = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+   collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+   collectCoverageFrom: ["**/src/*.{js,jsx}", "!**/node_modules/**", "!**/vendor/**"],
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
+  coverageDirectory: "./coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
    coveragePathIgnorePatterns: [
@@ -86,10 +86,10 @@ module.exports = {
   // modulePathIgnorePatterns: [],
 
   // Activates notifications for test results
-  // notify: false,
+   notify: true,
 
   // An enum that specifies notification mode. Requires { notify: true }
-  // notifyMode: "failure-change",
+   notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
   // preset: undefined,
@@ -113,7 +113,7 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-   rootDir: "./test",
+  // rootDir: "./test",
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
